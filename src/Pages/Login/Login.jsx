fda9864a109880954253a9690f1fbc964983eb8f
heetkumar <heetkumarpatel@gmail.com>
2021-09-24 01:51:54 +0530

    Basic code updated

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.scss';
+
+function Login() {
+    return(
+        <div className="head">
+           hector1
+        </div>
+    );
+}
+
+export default Login;
