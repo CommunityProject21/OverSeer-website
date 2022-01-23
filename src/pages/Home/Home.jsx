10d8d1a1703c8bba8556c8ea1d4a1bf23efe4d60
heet-kumar <heetkumarpatel@gmail.com>
2022-01-23 15:02:11 +0530

    manisha branch setup is done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return(
         <div>
-            Home Page in Heet's Branch
+            Home Page
         </div>
     );    
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Login() {
     return(
         <div>
-            Login Page
+            Login Page manisha branch
         </div>
     );
 }
