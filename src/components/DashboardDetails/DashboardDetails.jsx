cd0684e95babf33e345d1dd7683882725b45f090
heet-kumar <heetkumarpatel@gmail.com>
2022-01-28 15:38:12 +0530

    Dashboard Basic details UI Done

---
+++ src/components/DashboardDetails/DashboardDetails.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import {Typography,TextField,Button} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const DashboardDetails = () => {
+    return(
+        <div>
+            <Box sx={{width: '100%'}}>
+                <Typography variant="h4" component="div" gutterBottom sx={{fontWeight: 900,m:2,ml:3,color: '#6c757d'}}>
+                    OverSeer DashBoard
+                </Typography>
+            </Box>
+            <Box
+                 component="form"
+                 sx={{
+                     '& .MuiTextField-root': { m: 1, width: '45%' },
+                     display: 'flex',
+                     justifyContent: 'center',
+                     alignItems: 'center',
+                     mt: 10,
+                 }}
+                 noValidate
+                 autoComplete="off"
+            >
+                 <TextField 
+                     sx={{backgroundColor: '#fff', borderColor: '#000', borderRadius: 2}}
+                     id="outlined-search" 
+                     label="Enter the Patient ID" 
+                     type="search" 
+                 />
+                 <Button variant="contained" size="large" sx={{ my:1.2, fontSize: 'large', fontWeight: 'bold'}} startIcon={<SearchIcon />}>Search</Button>
+            </Box>
+            <Box sx={{display: 'flex', gap: 1, flexDirection: 'column', mt: 3, alignItems: 'center', }}>
+                 <Box sx={{display: 'flex', width: '55%', }}>
+                     <Typography sx={{ px: 2, fontSize: '120%'}} style={{fontWeight: '900'}}>
+                         Patient ID :
+                     </Typography>
+                     <Typography sx={{ fontSize: '120%',fontWeight: 'medium' }}>
+                         
+                     </Typography>
+                 </Box>
+                 <Box sx={{display: 'flex', width: '55%', }}>
+                     <Typography sx={{ px: 2, fontSize: '120%'}} style={{fontWeight: '900'}}>
+                         Patient Name :
+                     </Typography>
+                     <Typography sx={{ fontSize: '120%',fontWeight: 'medium' }}>
+                         
+                     </Typography>
+                 </Box>
+             </Box>
+        </div>
+    );
+}
+
+export default DashboardDetails;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import DashboardDetails from '../../components/DashboardDetails/DashboardDetails'
 
 const Home = () => {
     return(
-        <div>
-            Home Page with tested as Mateial UI working.
-            <div><Button variant="contained">Hello World</Button></div>
-        </div>
+        <DashboardDetails />
     );    
 }
 
