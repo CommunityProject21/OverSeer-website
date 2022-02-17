c46c604a37d0b174b38514506a62e5bd2d8f19ea
heet-kumar <heetkumarpatel@gmail.com>
2022-02-17 23:36:48 +0530

    Monitoring,PatientAllergies,PatientCondition,PatientObservation components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import DashboardDetails from '../../components/DashboardDetails/DashboardDetails'
-import PersonalDetails from '../../components/PersonalDetails/PersonalDetails'
+import DashboardDetails from '../../components/DashboardDetails/DashboardDetails';
+import PersonalDetails from '../../components/PersonalDetails/PersonalDetails';
+import PatientObservation from '../../components/PatientObservations/PatientObservations';
+import PatientAllergies from '../../components/PatientAllergies/PatientAllergies';
+import PatientCondition from '../../components/PatientCondition/PatientCondition';
+import MonitoringDevices from '../../components/MonitoringDevices/MonitoringDevices';
 
 const Home = () => {
     return(
         <div style={{display: 'flex', flexDirection: 'column', gap: 25}}>
             <DashboardDetails />
-            <PersonalDetails />
+            <MonitoringDevices />
         </div>
     );    
 }
