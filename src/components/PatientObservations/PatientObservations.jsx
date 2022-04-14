59c44b10c505e55a6aa80e86ac35feb47b29aa8a
heet-kumar <heetkumarpatel@gmail.com>
2022-04-14 18:26:55 +0530

    Patient Observation api implemented Successfully

--- src/components/PatientObservations/PatientObservations.jsx
+++ src/components/PatientObservations/PatientObservations.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const PatientObservation = () => {
 
     const [data,setData] = useState({});
-
+    var cnt = 0;
     useEffect(() => {
         const newid = JSON.parse(localStorage.getItem('user'));
         axios({
@@ -20,25 +20,28 @@ const PatientObservation = () => {
         <div>
             <Box sx={{ display: 'flex',justifyContent: 'center', width: '100%'}}>
                 <Box sx={{width: '60%', display: 'flex',flexDirection: 'column',justifyContent: 'flex-start'}}>
-                    <Typography component="div" variant="h5" sx={{fontWeight: 'bold',mb:4}}>Medical Details of the Patient :-</Typography>
+                    <Typography component="div" variant="h5" sx={{fontWeight: 'bold',mb:1}}>Medical Details of the Patient :-</Typography>
                     {
-                        data.observation !== undefined && data.observation.map(prop => {
-                            return (
-                                <Box sx={{display: 'flex',flexDirection: 'column'}}>
-
+                        data.observation !== undefined && data.observation.map((prop) => {
+                            // console.log(prop.observations);
+                            return(
+                                <Box>
                                     {
-                                        prop.observations.length > 3 && Object.keys(prop.observations).map((key, i) => {
-                                            return (
-                                                <p key={i}>
-                                                    <span>Key Name: {key}</span>
-                                                    <span>Value: {prop.observations[key]}</span>
-                                                </p>
+                                        (Object.keys(prop.observations).length > 3)?<Typography sx={{mt:3,mb:1,fontWeight: 'bold'}} component="div" variant="h5">Observation {++cnt}</Typography>:<></>
+                                    }
+                                    {
+                                        Object.keys(prop.observations).length > 3 && Object.keys(prop.observations).map((key, i) => {
+                                            return(
+                                                <Box component="div" variant="h4" key={i}>
+                                                    <Typography component="span" sx={{fontSize:'120%',fontWeight:'bolder'}}>{key}: </Typography>
+                                                    <Typography component="span" sx={{fontSize:'120%'}} >{prop.observations[key]}</Typography>
+                                                </Box>
                                             );
                                         })
                                     }
-                                    
                                 </Box>
                             );
+                            
                         })
                     }
                 </Box>
