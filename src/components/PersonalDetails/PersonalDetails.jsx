a32924f695ff153de489cf5d0afe13cebaa6bdc4
heet-kumar <heetkumarpatel@gmail.com>
2022-04-14 20:16:45 +0530

    personal details api implemented

--- src/components/PersonalDetails/PersonalDetails.jsx
+++ src/components/PersonalDetails/PersonalDetails.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const PersonalDetails = () => {
 
     //const [id,setid] = useState({})
-    const [data,setData] = useState({});
+    const [data,setData] = useState();
 
     // useEffect(() => {
     //     localStorage.setItem('user',JSON.stringify({}));
@@ -28,27 +28,30 @@ const PersonalDetails = () => {
             <Box sx={{ display: 'flex',justifyContent: 'center', width: '100%'}}>
                 <Box sx={{width: '60%', display: 'flex',flexDirection: 'column',justifyContent: 'flex-start'}}>
                     <Typography component="div" variant="h5" sx={{fontWeight: 'bold',mb:4}}>Personal Details of the Patient :-</Typography>
-                    <Box sx={{display: 'flex',flexDirection: 'column'}}>
-                        <Typography className="addpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
-                            <b>Address of the Patient :</b> {data.address}
-                        </Typography>
+                    {
+                        (data !== undefined)?
+                        <Box sx={{display: 'flex',flexDirection: 'column'}}>
+                            <Typography className="addpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
+                                <b>Address of the Patient :</b> {data.address}
+                            </Typography>
 
-                        <Typography className="zippd"component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
-                            <b>Zip Code :</b>&nbsp;{data.zipcode}
-                        </Typography>
+                            <Typography className="zippd"component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
+                                <b>Zip Code :</b>&nbsp;{data.zipcode}
+                            </Typography>
 
-                        <Typography className="genpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
-                            <b>Gender :</b>&nbsp;{data.gender} 
-                        </Typography>
+                            <Typography className="genpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
+                                <b>Gender :</b>&nbsp;{data.gender} 
+                            </Typography>
 
-                        <Typography className="racpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
-                            <b>Race of the patient :</b>&nbsp;{data.race}
-                        </Typography>
+                            <Typography className="racpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
+                                <b>Race of the patient :</b>&nbsp;{data.race}
+                            </Typography>
 
-                        <Typography className="ethpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
-                            <b>Ethnicity of the patient :</b>&nbsp;{data.ethnicity}
-                        </Typography>
-                    </Box>                
+                            <Typography className="ethpd" component="div" variant="body1" sx={{fontSize: '115%',mb:1}}>
+                                <b>Ethnicity of the patient :</b>&nbsp;{data.ethnicity}
+                            </Typography>
+                        </Box>:<></>
+                    }                
                 </Box>
             </Box> 
         </div>
